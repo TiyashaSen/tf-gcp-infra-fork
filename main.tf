1308c84a7f00ec898cb976653009252471f6c4b8
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-14 22:42:16 -0500

    Updated infrastructure as code with terraform

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+
+}
+
+
+resource "google_compute_network" "vpc_network" {
+  for_each                        = { for idx, name in var.vpc_names : name => idx }
+  name                            = each.key
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  delete_default_routes_on_create = true
+}
+
+
+resource "google_compute_subnetwork" "subnet_webapp" {
+  for_each      = google_compute_network.vpc_network
+  name          = "${var.subnetwebapp-name}-${each.value.name}"
+  network       = each.value.name
+  ip_cidr_range = var.ip-cidr-range-subnetwebapp
+  region        = var.region
+}
+
+resource "google_compute_subnetwork" "subnet_db" {
+  for_each      = google_compute_network.vpc_network
+  name          = "${var.subnetdb-name}-${each.value.name}"
+  network       = each.value.name
+  ip_cidr_range = var.ip-cidr-range-subnetdb
+  region        = var.region
+}
+
+resource "google_compute_route" "webapp-route" {
+  for_each         = google_compute_network.vpc_network
+  name             = "route-${each.value.name}"
+  dest_range       = var.dest-range
+  network          = each.value.name
+  next_hop_gateway = var.hop_gateway
+  priority         = var.webapp-route-priority
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "credentials" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "network-name" {
+  type = string
+
+}
+variable "subnetwebapp-name" {
+  type = string
+
+}
+variable "subnetdb-name" {
+  type = string
+
+}
+variable "dest-range" {
+  type = string
+
+}
+variable "ip-cidr-range-subnetwebapp" {
+  type = string
+}
+variable "ip-cidr-range-subnetdb" {
+  type = string
+}
+variable "webapp-route-name" {
+  type = string
+}
+variable "webapp-route-priority" {
+  type = number
+
+}
+variable "vpc_names" {
+  type = list(string)
+
+}
+
+variable "hop_gateway" {
+  type = string
+}
